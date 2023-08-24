1a15d9fbdcdbcae650d785f40f12e754ef603c44
wulandarisri268 <wulandarisri268@gmail.com>
2023-08-24 13:31:49 +0700

    feat: cache asset (font) from cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{js,jsx}"],
+    theme: {
+        fontFamily: {
+            sans: ['"Josefin Sans"'],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
